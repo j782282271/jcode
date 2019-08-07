8d3543475f90468ed61883291f6a3730f7674c34
jiangyang <782282271@qq.com>
2019-08-07 15:10:38 +0800

    时间map

--- src/main/java/com/jy/leetcode/timemap/TimeMap0.java
+++ src/main/java/com/jy/leetcode/timemap/TimeMap0.java
@@ -7,29 +7,19 @@ import java.util.TreeMap;
 /**
  * Created by jiangyang on 2019/8/5.
  * https://leetcode-cn.com/problems/time-based-key-value-store/
+ * 45个用例，第44个超时
  */
-class TimeMap {
+class TimeMap0 {
 
-    /**
-     * key-timestamp-value
-     */
     private Map<String, TreeMap<Integer, String>> map = new HashMap<>();
-    /**
-     * key-lastValue
-     */
-    private Map<String, String> lastValue = new HashMap<>();
 
-    public TimeMap() {
+    public TimeMap0() {
 
     }
 
     public void set(String key, String value, int timestamp) {
-        String lastV = lastValue.get(key);
-        if (lastV != null && lastV.equals(value)) {
-            return;
-        }
-        map.computeIfAbsent(key, (k) -> new TreeMap<>()).put(timestamp, value);
-        lastValue.put(key, value);
+        TreeMap treeMap = map.computeIfAbsent(key, (k) -> new TreeMap());
+        treeMap.put(timestamp, value);
     }
 
     public String get(String key, int timestamp) {
@@ -50,12 +40,10 @@ class TimeMap {
 //        [null, null, null, "", "", "", "low", "low"]
 //        [null, null, null, "", "high", "high", "low", "low"]
 
-        TimeMap timeMap = new TimeMap();
-        timeMap.set("love", "high", 10);
-        timeMap.set("love", "low", 20);
-        timeMap.set("love", "high", 30);
-        String val1 = timeMap.get("love", 5);
-        String val2 = timeMap.get("love", 10);
-        String val3 = timeMap.get("love", 31);
+        TimeMap0 timeMap0 = new TimeMap0();
+        timeMap0.set("love", "high", 10);
+        timeMap0.set("love", "low", 20);
+        String val1 = timeMap0.get("love", 5);
+        String val2 = timeMap0.get("love", 10);
     }
 }
