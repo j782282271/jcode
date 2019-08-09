f96d4f9adfafa9d6d466b475ff4bc164f6aee13a
jiangyang <782282271@qq.com>
2019-08-09 17:43:02 +0800

    sprng

--- src/main/java/com/jy/spring/App.java
+++ src/main/java/com/jy/spring/App.java
@@ -1,14 +1,23 @@
 package com.jy.spring;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by jiangyang on 2019/8/7.
  */
-public abstract class App {
+public abstract class App implements BeanFactoryPostProcessor {
     public abstract Command abc();
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath*:applicationContext.xml");
+        Command command = applicationContext.getBean(Command.class);
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println("App.postProcessBeanFactory");
     }
 }
--- src/main/java/com/jy/spring/Command.java
+++ src/main/java/com/jy/spring/Command.java
@@ -1,7 +1,19 @@
 package com.jy.spring;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by jiangyang on 2019/8/7.
  */
 public class Command {
+    private Map<String, String> map = new HashMap<>();
+
+    public Map<String, String> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, String> map) {
+        this.map = map;
+    }
 }
