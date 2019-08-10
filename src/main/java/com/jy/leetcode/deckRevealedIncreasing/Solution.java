1f8fdce3d900fa19c8e2335126f033414320f821
j782282271 <782282271@qq.com>
2019-08-10 10:45:46 +0800

    最小花费

---
+++ src/main/java/com/jy/leetcode/deckRevealedIncreasing/Solution.java
@@ -0,0 +1,32 @@
+package com.jy.leetcode.deckRevealedIncreasing;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * Created by Administrator on 2019/7/29.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] res = s.deckRevealedIncreasing(new int[]{17, 13, 11, 2, 3, 5, 7});
+    }
+
+    public int[] deckRevealedIncreasing(int[] deck) {
+        Arrays.sort(deck);
+        LinkedList<Integer> stack = new LinkedList<>();
+        int i = 0;
+        while (i < deck.length) {
+            if (stack.size() != 0) {
+                Integer last = stack.pollLast();
+                stack.addFirst(last);
+            }
+            stack.addFirst(deck[deck.length - 1 - i]);
+            i++;
+        }
+        for (int i1 = 0; i1 < stack.size(); i1++) {
+            deck[i1] = stack.get(i1);
+        }
+        return deck;
+    }
+}
--- src/main/java/com/jy/leetcode/minstairs/Solution.java
+++ src/main/java/com/jy/leetcode/minstairs/Solution.java
@@ -5,6 +5,12 @@ package com.jy.leetcode.minstairs;
  */
 class Solution {
 
+    public static void main(String[] args) {
+        int[] input = new int[]{0, 0, 1, 1};
+        Solution s = new Solution();
+        int res = s.minCostClimbingStairs(input);
+    }
+
     public int minCostClimbingStairs(int[] cost) {
         if (cost == null) {
             return 0;
@@ -13,17 +19,20 @@ class Solution {
     }
 
     public int minCostClimbingStairs(int[] cost, int end) {
-        if (end <= 1) {
-            return 0;
+        if (end == 1) {
+            return cost[0];
         }
         if (end == 2) {
-            return Math.min(cost[0], cost[1]);
+            return cost[1];
         }
         if (end == 3) {
-            return Math.min(cost[0] + cost[2], cost[1]);
+            return Math.min(cost[0] + cost[2], cost[1] + cost[2]);
         }
+
         int v1 = minCostClimbingStairs(cost, end - 2);
+        int res = Math.min(v1 + cost[end - 2], v1 + cost[end - 1]);
+
         int v2 = minCostClimbingStairs(cost, end - 1);
-        return Math.min(v1 + cost[end - 1], v2);
+        return Math.min(res, v2);
     }
 }
