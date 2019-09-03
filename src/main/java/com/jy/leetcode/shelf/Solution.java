a75dde0d8c81b67fddfaa6f5cdd8d637b4e3d34d
jiangyang <782282271@qq.com>
2019-09-03 15:53:37 +0800

    题解

--- src/main/java/com/jy/leetcode/shelf/Solution.java
+++ src/main/java/com/jy/leetcode/shelf/Solution.java
@@ -1,6 +1,7 @@
 package com.jy.leetcode.shelf;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,25 +11,47 @@ public class Solution {
 
     private static int SHELF_WIDTH;
 
+    public static void main(String[] args) {
+//        1求出总宽度除以shelf_width得到n至少需要n层
+//        2书按高度倒序，依次放入各层书架，超过某一层宽度则放到上层书架上，直到n层书架无法填放为止，保证上层书架的高度一定小于下层
+//        3无法填放书，则看n层书架剩余总宽度是否够放当前的书，如果不够则另起一层放当前的书
+//        4如果够放当前的书，移动当前n层书架内的书，原则：书只能从上层往下层填，优先把下层书架填满，空出上层书架，如果发现足够放当前书则停止
+//        5如果即使移动也无法空出空间则另起一层
+    }
+
     public int minHeightShelves(int[][] books, int shelf_width) {
         Solution.SHELF_WIDTH = shelf_width;
+        Arrays.sort(books, (o1, o2) -> o1[1] > o2[1] ? -1 : 1);
+//        int minLayer = (int) Math.ceil(getTotalW(books) / SHELF_WIDTH);
 
         Shelf shelf = new Shelf();
         addLayer(shelf, books[0][0], books[0][1]);
 
         for (int i = 0; i < books.length; ++i) {
-            Book book = new Book(books[i][0], books[i][1]);
-            Layer layer = shelf.getBestLayer(book);
-            if (layer != null) {
-                layer.add(book);
-                continue;
+            Layer lastLayer = shelf.get(shelf.size() - 1);
+            if (lastLayer.getW() + books[i][0] <= SHELF_WIDTH) {
+                lastLayer.add(new Book(books[i][0], books[i][1]));
+            } else if(shelf.getEmptySize()<books[i][0]){
+                addLayer(shelf, books[i][0], books[i][1]);
+            }else {
+
             }
         }
+        return 0;
     }
 
-    private static void addLayer(List<Layer> shelf, int h, int w) {
+
+    private double getTotalW(int[][] books) {
+        int res = 0;
+        for (int i = 0; i < books.length; ++i) {
+            res += books[i][0];
+        }
+        return res;
+    }
+
+    private static void addLayer(List<Layer> shelf, int w, int h) {
         Layer layer = new Layer();
-        layer.add(new Book(h, w));
+        layer.add(new Book(w, h));
         shelf.add(layer);
     }
 
@@ -36,9 +59,9 @@ public class Solution {
         int h;
         int w;
 
-        public Book(int h, int w) {
-            this.h = h;
+        public Book(int w, int h) {
             this.w = w;
+            this.h = h;
         }
     }
 
@@ -52,6 +75,14 @@ public class Solution {
                 }
             }
             return null;
+        }
+
+        public int getEmptySize() {
+            int res = 0;
+            for (Layer layer : this) {
+                res += SHELF_WIDTH - layer.getW();
+            }
+            return res;
         }
     }
 
