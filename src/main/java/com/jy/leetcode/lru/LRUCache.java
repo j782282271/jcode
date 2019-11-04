c4917eeccab448f6eb5c91244ef9a5191c0cf09f
jiangyang <782282271@qq.com>
2019-11-04 11:12:59 +0800

    lru

--- src/main/java/com/jy/leetcode/lru/LRUCache.java
+++ src/main/java/com/jy/leetcode/lru/LRUCache.java
@@ -1,28 +1,77 @@
 package com.jy.leetcode.lru;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Created by jiangyang on 2019/11/4.
  */
 public class LRUCache {
-    private LinkedHashMap<Integer, Integer> map;
+    private HashMap<Integer, Node> map;
+    private Node firstNode = new Node(-1, -1, null);
+    private Node lastNode = firstNode;
+    private int size;
 
     public LRUCache(int capacity) {
-        map = new LinkedHashMap<Integer, Integer>(capacity,0.75f,true) {
-            @Override
-            protected boolean removeEldestEntry(Map.Entry eldest) {
-                return size() > capacity;
-            }
-        };
+        size = capacity;
+        map = new HashMap<>(size);
     }
 
     public int get(int key) {
-        return map.getOrDefault(key, -1);
+        Node node = map.get(key);
+        if (node == null) {
+            return -1;
+        }
+        move2Last(node);
+        return node.value;
     }
 
     public void put(int key, int value) {
-        map.put(key, value);
+        if (!map.containsKey(key) && map.size() >= size) {
+            Node secd = firstNode.next;
+            map.remove(secd.key);
+            firstNode.next = secd.next;
+            if (secd.next != null) {
+                secd.next.pre = firstNode;
+            }
+        }
+        Node curr = map.get(key);
+        if (curr == null) {
+            curr = new Node(key, value, lastNode);
+            appendLast(curr);
+            map.put(key, curr);
+        } else {
+            move2Last(curr);
+            curr.value = value;
+        }
+    }
+
+    private class Node {
+        Integer key;
+        Integer value;
+        Node next;
+        Node pre;
+
+        public Node(Integer k, Integer v, Node p) {
+            key = k;
+            value = v;
+            pre = p;
+        }
+    }
+
+    private void move2Last(Node node) {
+        if (node == lastNode) {
+            return;
+        }
+        node.pre.next = node.next;
+        if (node.next != null) {
+            node.next.pre = node.pre;
+        }
+        appendLast(node);
+    }
+
+    private void appendLast(Node curr) {
+        lastNode.next = curr;
+        curr.pre = lastNode;
+        lastNode = curr;
     }
 }
---
+++ src/main/java/com/jy/leetcode/lru/LRUCache1.java
@@ -0,0 +1,28 @@
+package com.jy.leetcode.lru;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Created by jiangyang on 2019/11/4.
+ */
+public class LRUCache1 {
+    private LinkedHashMap<Integer, Integer> map;
+
+    public LRUCache1(int capacity) {
+        map = new LinkedHashMap<Integer, Integer>(capacity,0.75f,true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return size() > capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return map.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        map.put(key, value);
+    }
+}
